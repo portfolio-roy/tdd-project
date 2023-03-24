68ea8022f7e0924e2b5606c4fb185ec2c94eea03
JacintaNgwu <jacintangwu9900@gmail.com>
2023-03-24 15:08:41 +0530

    Initialize an instance of solver and write tests for the factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,4 +3,16 @@ require './solver'
 describe Solver do
     # initialize an instance of Solver class
     # write tests for the factorial method
+    solver = Solver.new
+    describe "#factorial" do
+        it "should return error when number is negative" do
+            expect(solver.factorial(-3)).to raise_error(Exception)
+        end
+        it "should return 1 when given 0" do
+            expect(solver.factorial(0)).to eq(1)
+        end
+        it "should return 120 when given 5" do
+            expect(solver.factorial(5)).to eq(120)
+        end
+    end
 end
