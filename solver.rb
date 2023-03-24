385a76e306b087b053ad289020067c582cb9e963
Utsargo Roy <royutsargo@gmail.com>
2023-03-24 20:49:40 +0600

    Define the fizzbuzz method and run the tests

--- solver.rb
+++ solver.rb
@@ -17,4 +17,16 @@ class Solver
     def reverse(string)
         string.reverse
     end
+
+    def fizzbuzz(number)
+        if (number % 15).zero?
+            'fizzbuzz'
+        elsif (number % 3).zero?
+            'fizz'
+        elsif (number % 5).zero?
+            'buzz'
+        else
+            number
+        end
+    end
 end
